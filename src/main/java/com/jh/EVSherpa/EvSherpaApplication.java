a2cdd7f4e47f07e8db5ea06c4c51f5e65651a768
LeeJooHwan <70500003+JHwan96@users.noreply.github.com>
2024-03-19 02:57:12 +0900

    Initial commit

---
+++ src/main/java/com/jh/EVSherpa/EvSherpaApplication.java
@@ -0,0 +1,13 @@
+package com.jh.EVSherpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvSherpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvSherpaApplication.class, args);
+	}
+
+}
